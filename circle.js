5b581c096acb48635f48f2ac81f1ee38a8e7010a
Nasif Qadri <nsq263@gmail.com>
2023-11-13 02:31:39 -0700

    lkjfpoiesf;lsekf;s

--- circle.js
+++ circle.js
@@ -1,16 +1,24 @@
-const tolerance = 7;
+const tolerance = 5;
 
 let centerX;
 let centerY;
 let r;
 let dists;
+let t;
+let error =0;
+let last = 0;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background(0x00);
     frameRate(60);
     centerX = width / 2;
-    centerY = height/2;
+    centerY = height / 2;
+    let font = loadFont('./VPPixel-Simplified.otf');
+    textFont(font)
+    textSize(35)
+    textAlign(CENTER, CENTER);
+    t = "";
 }
 
 function draw() {
@@ -34,10 +42,20 @@ function mousePressed() {
     circle(centerX, centerY, r * 2);
     
     dists = [];
+    push();
 }
 
+function getError(list, target) {
+    let error = 0;
+    sum = list.reduce((x, y) =>
+        (x + y)
+    );
+    error = sum/target;
+    return error;
+}
 
 function mouseDragged() {
+
     let x = mouseX - centerX;
     let y = mouseY - centerY;
     let d = Math.abs(r - Math.sqrt(x ** 2 + y ** 2));
@@ -58,14 +76,21 @@ function mouseDragged() {
         red = 0xFF;
     }
     
-
     fill(red,green,0x00);
     noStroke();
     circle(mouseX, mouseY, 20-(dNorm*3));
     
-    dists.push(d);
+    dists.push(1 - dNorm / 2);
+    // console.log(1 - dNorm/2)
+    last = error;
+    error = getError(dists, dists.length);
+    stroke(0x00);
+    fill(0x00);
+    text((last * 100).toFixed(2), width / 2, width / 2 - 25);
+    fill(0xFF);
+    t = text((error*100).toFixed(2), width / 2, width / 2 - 25);
 
-    
+
 
 }
 
